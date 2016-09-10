cfd85a49ca190b29ed2b3233ee2779e8455645b4
KurtWeber <weberkrt@msn.com>
2016-09-10 12:20:11 -0400

    Backing up

--- ProjectFortuneTeller/Program.cs
+++ ProjectFortuneTeller/Program.cs
@@ -10,6 +10,7 @@ namespace ProjectFortuneTeller
     {
         static void Main(string[] args)
         {
+            //Initialize input variables
             string firstName;
             string lastName;
             int age;
@@ -17,31 +18,70 @@ namespace ProjectFortuneTeller
             string roygbiv;
             int numSiblings;
 
+            //Initialize misc variables
+            int retYrs;
+            string vacHome;
+
+            //User input
             Console.Write("Welcome to my fortune teller App.  With just a few details, I can tell you your furture." + "\n" + "\n");
             Console.Write("Please enter your first name: ");
             firstName = Console.ReadLine();
-            Console.WriteLine("\n" + firstName); //  testing variable
 
             Console.Write("\n" + "Please enter your last name: ");
             lastName = Console.ReadLine();
-            Console.WriteLine("\n" + lastName); // testing variable
 
             Console.Write("\n" + "Please enter your age in years: ");
             age = int.Parse(Console.ReadLine());
-            Console.WriteLine("\n" + age); // testing variable
 
             Console.Write("\n" + "Please enter your birth month: ");
             birthMonth = int.Parse(Console.ReadLine());
-            Console.WriteLine("\n" + birthMonth); // testing variable
 
             Console.Write("\n" + "Please enter your favorite ROYGBIV color: ");
             roygbiv = Console.ReadLine();
-            Console.WriteLine("\n" + roygbiv);
 
             Console.Write("\n" + "Please enter your number of sibling: ");
             numSiblings = int.Parse(Console.ReadLine());
-            Console.WriteLine("\n" + numSiblings);
 
+            // Age odd/even check
+            if (age / 2 != 0)
+            {
+                retYrs = 35; //Odd age
+            }
+            else
+            {
+                retYrs = 20; //Even age
+            }
+            Console.WriteLine("\n" + retYrs);  //Check age
+
+            //Set vacation home
+            if (numSiblings == 0)
+            {
+                vacHome = "Hawaii";
+            }
+            else if (numSiblings == 1)
+            {
+                vacHome = "Florida Keys";
+            }
+            else if (numSiblings == 2)
+            {
+                vacHome = "San Diego";
+            }
+            else if (numSiblings == 3)
+            {
+                vacHome = "Martha's Vineyard";
+            }
+            else if (numSiblings > 3)
+            {
+                vacHome = "Paris";
+            }
+            else
+            {
+                vacHome = "parents basement";
+            }
+
+            //Set vehicle type
+
+        
             Console.ReadKey();
         }
     }
